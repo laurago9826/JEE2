26cd9e45c2fab873751db3cc722d57646911f7d6
laurago9826 <laura.guljas@gmail.com>
2019-11-29 13:49:09 +0100

    Implemented loading of static files

--- sportbetting-application-web/src/main/java/com/sportbettings/config/MvcConfig.java
+++ sportbetting-application-web/src/main/java/com/sportbettings/config/MvcConfig.java
@@ -5,13 +5,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.sportbettings")
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
+				.setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver());
+	}
 
 	@Bean
 	public ViewResolver viewResolver() {
--- sportbetting-application-web/src/main/webapp/WEB-INF/view/homepage.jsp
+++ sportbetting-application-web/src/main/webapp/WEB-INF/view/homepage.jsp
@@ -1,22 +1,12 @@
-<%-- <%@ page language="java" contentType="text/html; charset=ISO-8859-1" --%>
-<%--     pageEncoding="ISO-8859-1"%> --%>
-<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> -->
-<!-- <html> -->
-<!-- <head> -->
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"> -->
-<!-- <title>HelloWorld page</title> -->
-<!-- </head> -->
-<!-- <body> -->
-<%--     Greeting : ${greeting} --%>
-<!-- </body> -->
-<!-- </html> -->
 <!DOCTYPE HTML>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html xmlns:th="http://www.thymeleaf.org">
 <head>
-    <title>Getting Started: Serving Web Content</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="<c:url value="/resources/css/styles.css" />" rel="stylesheet">
+	<title>Getting Started: Serving Web Content</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
-    <p>"Hello, ${name} !" </p>
+	<p class="test">"Hello, ${name} !"</p>
 </body>
 </html>
